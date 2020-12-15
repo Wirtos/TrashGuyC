510356ad22a834dcab7f6d95796230830821eefe
Wirtos_new <Wirtos.new@gmail.com>
2020-12-15 12:25:33 +0200

    Add support for multiple arguments

--- main.c
+++ main.c
@@ -18,13 +18,30 @@ void tsleep(long int ms) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        puts("Input a string!");
+    TrashGuyState st;
+    if (argc == 2) {
+        st = tguy_init_str(argv[1], strlen(argv[1]), 4);
+    } else if (argc > 2) {
+        size_t len = (argc - 1) * 2 - 1;
+        CString *cs_arr = calloc(len, sizeof(*cs_arr));
+        if (!cs_arr) {
+            puts("Not enough memory!");
+            return EXIT_FAILURE;
+        }
+        for (int i = 1, c = 0; i < argc; i++, c += 2) {
+            cs_arr[c] = (CString) {argv[i], strlen(argv[i])};
+            if (i + 1 != argc) {
+                cs_arr[c + 1] = (CString) {" ", 1};
+            }
+        }
+        st = tguy_init_arr(cs_arr, len, 4);
+        free(cs_arr);
+    } else {
+        puts("Provide at least one string!");
         return EXIT_FAILURE;
     }
     setvbuf(stdout, NULL, _IONBF, 0);
     {
-        TrashGuyState st = tguy_init_str(argv[1], strlen(argv[1]), 4);
         for (int i = 0; i < st.max_frames; i++) {
             tguy_from_frame(&st, i);
             tguy_print(&st);

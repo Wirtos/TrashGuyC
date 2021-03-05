963da30d9bea931efdf70ce0d49ada663876da0d
Wirtos_new <Wirtos.new@gmail.com>
2021-03-05 10:30:24 +0200

    [0.1.0] Updated grapheme-complete libtguy

--- main.c
+++ main.c
@@ -20,21 +20,20 @@ void tsleep(long int ms) {
 int main(int argc, char *argv[]) {
     TrashGuyState *st;
     if (argc == 2) {
-        st = tguy_init_str(argv[1], strlen(argv[1]), 4);
+        st = tguy_from_utf8(argv[1], strlen(argv[1]), 4);
     } else if (argc > 2) {
-        size_t len = (argc - 1) * 2 - 1;
-        CString *cs_arr = calloc(len, sizeof(*cs_arr));
+        size_t len = (argc - 1);
+        TGString *cs_arr = calloc(len, sizeof(*cs_arr));
         if (!cs_arr) {
             puts("Not enough memory!");
             return EXIT_FAILURE;
         }
-        for (int i = 1, c = 0; i < argc; i++, c += 2) {
-            cs_arr[c] = (CString) {argv[i], strlen(argv[i])};
-            if (i + 1 != argc) {
-                cs_arr[c + 1] = (CString) {" ", 1};
-            }
+        for (int i = 1; i < argc; i++) {
+            size_t slen = strlen(argv[i]);
+            argv[i][slen] = ' ';
+            cs_arr[i - 1] = (TGString) {argv[i],  slen + 1};
         }
-        st = tguy_init_arr(cs_arr, len, 4);
+        st = tguy_from_arr(cs_arr, len, 4);
         free(cs_arr);
     } else {
         puts("Provide at least one string!");
@@ -43,9 +42,9 @@ int main(int argc, char *argv[]) {
     setvbuf(stdout, NULL, _IONBF, 0);
     {
         for (int i = 0, frames = tguy_get_frames_count(st); i < frames; i++) {
-            tguy_from_frame(st, i);
+            tguy_set_frame(st, i);
             tguy_print(st);
-            putchar('\r');
+            putchar('\n');
             tsleep(500);
         }
     }
